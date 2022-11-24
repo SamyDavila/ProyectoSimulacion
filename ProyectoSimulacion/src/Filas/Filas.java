8f248bc4541d00f7f4a4d3797eab2d60324d2339
SamyDavila <106284805+SamyDavila@users.noreply.github.com>
2022-11-23 19:27:22 -0600

    Update Filas.java

--- ProyectoSimulacion/src/Filas/Filas.java
+++ ProyectoSimulacion/src/Filas/Filas.java
@@ -3,7 +3,7 @@ package Filas;
 import java.util.LinkedList;
 
 public class Filas {
-    static int n = 1; //Número de probadores
+    static int n = 4; //Número de probadores
     
     /**
      * Calcula el número de clientes que hay en la fila
@@ -28,23 +28,26 @@ public class Filas {
     }
     
     public static void main(String[] args){
+        int Inactividad = 0;
         int[] probadores = new int[n]; //Inicializamos los tiempos en que se desocupan los probadores
         LinkedList<Cliente> clientes = new LinkedList(); //Inicializamos el historial de clientes
         
         //Añadimos al primer cliente (Como no hay anterior le ponemos los valores en 0, y la lista de probadores inicializada
         clientes.add(new Cliente(0, 0));
         clientes.get(0).Todo(0, probadores.clone());
+        Inactividad += clientes.get(0).sInactividad;
         
         //Recorremos los clientes que vamos a analizar
-        for (int i = 1; i < 20; i++) {
+        for (int i = 1; i < 205; i++) {
             //Se agrega cada cliente al historial
             int x = nFila(clientes);
             clientes.add(new Cliente(i, x));
             clientes.get(i).Todo(clientes.get(i-1).eLlegadaFila, clientes.get(i-1).sSalidaProbadores.clone());
+            Inactividad += clientes.get(i).sInactividad;
         }
         
         //Imprimimos el historial
-        System.out.println(clientes);
+        //System.out.println(clientes);
         
         //Contamos el número de clientes que salieron de la fila por lo larga que estaba
         int cont = 0;
@@ -53,6 +56,6 @@ public class Filas {
                 cont++;
             }
         }
-        System.out.println("Se salieron un total de " + cont + " clientes.");
+        System.out.println("Se salieron un total de " + cont + " clientes.\nLos probadores estuvieron "+Inactividad+" unidades inactivos");
     }
 }
